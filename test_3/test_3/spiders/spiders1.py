9f47918f0a169146c7605ea2e5e8eface559057e
mohammad <aliakbariiimohammad@gmail.com>
2021-12-01 23:07:45 -0800

    giving color for each product and give box prices for product that has similar colors

--- test_3/test_3/spiders/spiders1.py
+++ test_3/test_3/spiders/spiders1.py
@@ -8,7 +8,7 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://www.digikala.com/product/dkp-446827/",
+            "https://www.digikala.com/product/dkp-6351381/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -19,24 +19,41 @@ class QuotesSpider(scrapy.Spider):
         information = re.findall("var variants =(.+?);\\n", response.body.decode('utf-8'))
         information = information[0]
         answer = json.loads(information)
+
         list_1 = []
         rates = []
         prices = []
+        colors = set()
+
         for i in answer:
             product = answer[i]
-            price = product["price_list"]["selling_price"]
+            price = product["price_list"]["selling_price"] / 10
             prices.append(price)
             lead_time = product["leadTime"]
             cancel_percentage = product["marketplace_seller"]["rating"]["cancel_percentage"]
             return_percentage = product["marketplace_seller"]["rating"]["return_percentage"]
             ship_on_time_percentage = product["marketplace_seller"]["rating"]["ship_on_time_percentage"]
+            if product["color"] != []:
+                color = product["color"]["title"]
+
+            else:
+                color = None
+            colors.add(color)
+
             rate = (cancel_percentage + ship_on_time_percentage + return_percentage) / 3
             rates.append(rate)
-            list_1.append((price, lead_time, rate))
-        max_rate = max(rates)
+
+            # هر tuple در list_1 به ترتیب تشکیل شده از قیمت,تعداد روز تحویل به مشتری,درصد رضایت و رنگ میباشد
+            list_1.append((price, lead_time, rate, color))
+
         box_price = list_1[0][0]
         box_objects = list_1[0]
-        # print(box_objects)
+
+        print(list_1)
+        colors = list(colors)
+        print(colors)
+
+        print(box_objects)
         print(f"box price:{list_1[0][0]}")
         list_1.pop(0)
 
@@ -45,20 +62,21 @@ class QuotesSpider(scrapy.Spider):
         your_answer = input("if you want minimum enter m or if you want box enter b?\n").lower()
         if your_answer == "b":
             for info in list_1:
+                if info[3] == box_objects[3]:
 
-                if box_objects[1] > info[1]:
+                    if box_objects[1] > info[1]:
 
-                    new_price = box_price + (info[0] * (info[1] * 0.05))
-                elif box_objects[1] < info[1]:
-                    new_price = box_price - (info[0] * (info[1] * 0.05))
-                else:
-                    new_price = info[0]
-                # if info[2] < box_objects[2]:
-                #     new_price = new_price - (box_price * ((box_objects[2] - (info[2])) * 0.01))
-                # elif info[2] > max_rate:
-                #     new_price = new_price + (box_price * ((box_objects[2] - (info[2])) * 0.01))
+                        new_price = box_price + (info[0] * (info[1] * 0.05))
+                    elif box_objects[1] < info[1]:
+                        new_price = box_price - (info[0] * (info[1] * 0.05))
+                    else:
+                        new_price = info[0]
+                    if info[2] < box_objects[2]:
+                        new_price = new_price - (box_price * ((box_objects[2] - (info[2])) * 0.01))
+                    elif info[2] > box_objects[2]:
+                        new_price = new_price + (box_price * ((box_objects[2] - (info[2])) * 0.01))
 
-                print(f"{info[0]}------>{round(new_price, 2)}")
+                    print(f"{info[0]}------>{round(new_price, 2)}")
 
 
         elif your_answer == "m":
